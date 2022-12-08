3617d4453cdda0189978417247e538b0b4c56ba5
Mohamed Zkrallah <54005330+muhammadzkralla@users.noreply.github.com>
2022-12-08 16:57:58 +0200

    Adding OnDestinationChangedListener

--- Task1/app/src/main/java/com/zkrallah/task1/MainActivity.kt
+++ Task1/app/src/main/java/com/zkrallah/task1/MainActivity.kt
@@ -1,28 +1,46 @@
 package com.zkrallah.task1
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
 import com.zkrallah.task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
+
+
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private val listener = NavController.OnDestinationChangedListener { navController, destination, arguments ->
+        Snackbar.make(binding.root, "YOU ARE IN ${destination.label} ", Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        navController.removeOnDestinationChangedListener(listener)
+        super.onPause()
     }
 }
